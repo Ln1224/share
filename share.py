86d4a2172bd9d98cf40ecaf3a97cfcff3226bb5f
Ln1224 <lnmag@yahoo.com>
2025-02-18 15:35:20 +0100

    Update share.py

--- share.py
+++ share.py
@@ -23,4 +23,5 @@ def calculate():
         return render_template('index.html', result=None, value=None, error=str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5001)
+
